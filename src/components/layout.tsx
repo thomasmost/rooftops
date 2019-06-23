a81f7390866a8c10d0eeb89a8043c0b8d6297cb8
thomas/NERO <tomconmo@gmail.com>
2019-06-23 14:07:19 -0400

    eggshell the bg a bit

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -25,7 +25,7 @@ const Layout = ({ children }) => (
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
             paddingTop: 0,
-            backgroundColor: `white`
+            backgroundColor: `#fffdfb`
           }}
         >
           <main>{children}</main>
