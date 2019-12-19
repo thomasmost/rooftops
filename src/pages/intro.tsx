2d67074e0404632d013ae54e09a3c0f6e6fc9251
thomas <thomas@abacus.com>
2019-12-18 22:48:41 -0500

    style tweaks and intro page

--- src/pages/intro.tsx
+++ src/pages/intro.tsx
@@ -5,8 +5,18 @@ import SEO from "../components/seo"
 
 const IntroPage = () => (
   <Layout>
-    <SEO title="Script" />
+    <SEO title="Intro" />
     <h1>Intro</h1>
+    <p>
+      A new class of person has appeared in the world.
+      Superhumans roam the streets, maraud the bay, and patrol the sky.
+      Uncommon but no longer unthinkable,
+      they lurk in caverns and alleyways,
+      on rocky cliffs and <em>Rooftops</em>.
+    </p>
+    <p>
+      The city needs heroes. But what manner of hero will you be?
+    </p>
   </Layout>
 );
 
