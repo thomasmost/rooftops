fc78dbba1dfbf7113a40e8eb54aa8d96c2eb9950
thomas/NERO <tomconmo@gmail.com>
2020-03-01 23:09:31 -0500

    updated quickplay guide to v.2

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -9,13 +9,17 @@ const DownloadPage = () => (
     <h1>Download</h1>
     <article>
       <p>
-        The current Quickplay Guide is on <strong>version 1</strong>.
+        The current Quickplay Guide is on <strong>version 2</strong>.
       </p>
       <p>
-        <a href="https://my.pcloud.com/publink/show?code=XZkuQDkZBILaaorFEu02JFp5rF5vvzwPljuy">
+        <a href="https://my.pcloud.com/publink/show?code=XZd3C1kZxa2GazJw3KH1C8W8qnk9ypL2A2aX">
           Download it now
         </a>
         <span> and begin your adventure...</span>
+      </p>
+      <p>
+         <strong>Note that some of the sheets in this quickplay have been altered to get people up and running fast.</strong>
+         If you have any questions, contact us through our <a href="https://www.kickstarter.com/projects/1303651095/rooftops-the-world-is-watching-a-superhero-rpg">Kickstarter campaign!</a>
       </p>
     </article>
   </Layout>
