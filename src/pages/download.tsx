f0a2dacf3ecbeedc77b0c01eff5a869fe73e5b96
thomas/NERO <tomconmo@gmail.com>
2020-03-01 23:11:18 -0500

    spacing fix

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -18,7 +18,7 @@ const DownloadPage = () => (
         <span> and begin your adventure...</span>
       </p>
       <p>
-         <strong>Note that some of the sheets in this quickplay have been altered to get people up and running fast.</strong>
+         <strong>Note that some of the sheets in this quickplay have been altered to get people up and running fast. </strong>
          If you have any questions, contact us through our <a href="https://www.kickstarter.com/projects/1303651095/rooftops-the-world-is-watching-a-superhero-rpg">Kickstarter campaign!</a>
       </p>
     </article>
